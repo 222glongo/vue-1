16bd77f4bc28fedf5be3680abebbb89045d3391b
222glongo <62153207+222glongo@users.noreply.github.com>
2022-03-01 09:04:13 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+//console.log("hello, vue");
+const app = Vue.createApp({
+  data(){
+    return{
+    url: 'http://www.thenetninja.co.uk',
+    showBooks: true,
+    books; [
+      {title:'name of the wind', author: 'patrick rothfuss', img: 'assests/1.jpg', isFav: true},
+      {title:'the way of kings', author: 'brandon sanderson', img: 'assests/2.jpg', isFav: false},
+      {title:'the final empire', author: 'brandon sanderson', img: 'assests/1.jpg', isFav: true},
+      ]
+    }
+  },
+  methods: {
+    toggleShowBooks(){
+      this.showBooks = !this.showBooks
+    },
+    toggleFav(book) {
+      book.isFav = !book.isFav
+    }
+  },
+  computed:{
+    filteredBooks{
+      return this.books.filter((book) => book.isFav)
+    }
+  }
+})
+
+app.mount('#app')
